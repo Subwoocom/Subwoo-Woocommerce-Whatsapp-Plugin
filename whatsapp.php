5d53208728f02adab0c062a78d8edf8d3d37d510
Subwoocom <123628556+Subwoocom@users.noreply.github.com>
2023-01-26 12:09:02 +0300

    Test

---
+++ whatsapp.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Plugin Name:       Subwoo Whatsapp Paketi
+ * Plugin URI:        https://subwoo.com
+ * Description:       Subwoo sizlere müşterilerinize ulaşabileceğiniz whatsapp hizmeti sunar.
+ * Version:           1.0
+ * Requires at least: 5.6
+ * Requires PHP:      5.6
+ * Author:            Subwoo
+ * Author URI:        https://subwoo.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/Subwoocom/Subwoo-Woocommerce-Whatsapp-Plugin
+ * Text Domain:       whatsapp
+ */
+defined('ABSPATH') or die('Wrong way dude');
+
+function KRGTKP_add_metabox(){
+    global $woocommerce, $order, $post;
+    add_meta_box(
+        "whatsapp",
+        "Whatsapp Mesaj",
+        "whatsapp_send_message",
+        "shop_order",
+        "side",
+        "high"
+    );
+}
+add_action("add_meta_boxes","KRGTKP_add_metabox");
+
+function whatsapp_send_message($order){
+    $order = $order->ID;
+    $order =  wc_get_order($order); // Order id
+
+    $address = $order->get_address('billing');
+    $phone = $address['phone'];
+
+    $firsr_char = $phone[0];
+
+    if($firsr_char == '9'){
+        $phone = $phone;
+    }
+    else{$phone =  '9'.$phone;}
+    ?>
+    Mesaj Gönderilecek Numara Gönderim Ardersinden Çekilmektedir.
+    <a target="_blank" href="https://wa.me/<?php echo $phone ?>" class="button" style="background-color: green;color: white; margin-top: 10px"> Mesaj Gönder</a>
+    <?php
+}
+
+
+function display_banner_notice(){
+    $banner_img_url = plugin_dir_url( __FILE__ ) . 'subwoo-plugins.png';
+    echo '<div class="notice notice-info is-dismissible" id="banner-notice">';
+    echo '<img src="' . $banner_img_url . '" alt="Reklam Banneri">';
+    echo '</div>';
+    echo '<style>
+        #banner-notice {
+            background-color: #377dff;
+            padding: 1em;
+            text-align: center;
+        }
+        #banner-notice img {
+            max-width: 100%;
+            height: 100px;
+        }
+    </style>';
+}
+add_action( 'admin_notices', 'display_banner_notice' );

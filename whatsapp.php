4a5ad3e25c4d10f1f4735442817ebd04c2a8d0c9
Subwoocom <123628556+Subwoocom@users.noreply.github.com>
2023-01-30 16:19:35 +0300

    Update whatsapp.php

--- whatsapp.php
+++ whatsapp.php
@@ -50,7 +50,7 @@ function whatsapp_send_message($order){
 function display_banner_notice(){
     $banner_img_url = plugin_dir_url( __FILE__ ) . 'subwoo.jpg';
     echo '<div class="notice notice-info is-dismissible" id="banner-notice">';
-    echo '<img href="subwoo.com" src="' . $banner_img_url . '" alt="Subwoo E-Commerce">';
+    echo '<iframe src="https://www.subwoo.com/plugins/" title="subwoo plugins"></iframe>';
     echo '</div>';
     echo '<style>
         #banner-notice {
